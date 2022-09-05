911395d3ac7a75a192b4a69bbbcbe2329134c7cb
Opliko <opliko.reg@protonmail.com>
2022-09-06 00:54:40 +0200

    fix: set the class to default export
    Currently this doesn't match the actual export and TS in an ES Module will make you `import { HKP } from "@openpgp/hkp-client"`, which will throw an error at runtime.

--- src/hkp.d.ts
+++ src/hkp.d.ts
@@ -1,4 +1,4 @@
-export class HKP {
+export default class HKP {
   constructor(keyServerBaseUrl?: string);
   public lookup(options: { keyid?: string, query?: string }): Promise<string | undefined>;
 }
